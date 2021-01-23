06ec272cc63edd6657ab36e867ee4497b3eb32b0
emm <723125@gmail.com>
2021-01-23 11:33:05 +0000

    Initial Commit

---
+++ splitApp_14/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  splitApp_14
+//
+//  Created by emm on 23/01/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ splitApp_14/splitApp_14App.swift
@@ -0,0 +1,17 @@
+//
+//  splitApp_14App.swift
+//  splitApp_14
+//
+//  Created by emm on 23/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct splitApp_14App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
